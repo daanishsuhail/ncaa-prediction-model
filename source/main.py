bc661be388d63eb0bac8e50e56ac0d59c43c9529
unknown <campilchard@gmail.com>
2021-03-21 14:12:00 -0500

    CSV dumper added; it outputs file 'team_avg_diffs.csv' in main folder

--- source/main.py
+++ source/main.py
@@ -1,6 +1,7 @@
 import avg_diff
 import sys
 import os
+import csv
 
 # this is the master file where all functions are going to be run.
 # do as little work as possible on this file, we'll can call them by classes later.
@@ -9,14 +10,16 @@ import os
 # function finds the average point difference. See avg_diff.py for more
 
 # differences - A dictionary set with the key as the teamcode and the value as the average point differential
-differences = {}
-print(os.path.abspath('data/team_data/'))
-for filename in os.listdir(os.path.abspath('data/team_data/')):
-    if filename.endswith(".csv"):
-        diff = avg_diff.difference_average(filename)
-        print(diff)
-        differences[filename.split('.')[0]] = diff
-    else:
-        continue
 
-print(differences)
+with open('team_avg_diffs.csv', 'w') as dump:
+    fieldnames = ["Teams", "Avg Point Diff"]
+    writer = csv.DictWriter(dump, fieldnames=fieldnames)
+
+    writer.writeheader()
+    for filename in os.listdir(os.path.abspath('data/team_data/')):
+        if filename.endswith(".csv"):
+            diff = avg_diff.difference_average(filename)
+            print(diff)
+            writer.writerow({"Teams": filename.split('.')[0], "Avg Point Diff": diff})
+        else:
+            continue
