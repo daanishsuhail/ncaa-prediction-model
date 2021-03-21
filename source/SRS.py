64548b8f8875f0767f54d4108bad8c415e79bae1
Camyo4650 <campilchard@gmail.com>
2021-03-21 15:53:28 -0500

    "Fixed SRS"

--- source/SRS.py
+++ source/SRS.py
@@ -6,8 +6,10 @@ avg_diffs = {}
 def SRS(team_avg_diffs, team):
     tad = open(team_avg_diffs, mode='r')
     reader = csv.DictReader(tad)
-    for k, v in reader:
-        avg_diffs[k] = v
-        print(k+', '+v)
+    l = list(reader)
+    avg_diffs = dict()
+    for d in l:
+        avg_diffs[d["Teams"]] = d["Avg Point Diff"]
     # initial value
     init = avg_diffs[team]
+    print(init)
--- source/main.py
+++ source/main.py
@@ -13,18 +13,22 @@ import platform
 
 # differences - A dictionary set with the key as the teamcode and the value as the average point differential
 
-with open('team_avg_diffs.csv', 'w') as dump:
-    fieldnames = ["Teams", "Avg Point Diff"]
-    writer = csv.DictWriter(dump, fieldnames=fieldnames)
-    linux_stringa = ''
-    linux_stringb = ''
-    if platform.system() == 'Linux':
-        linux_stringa = '../'
-    writer.writeheader()
-    for filename in os.listdir(os.path.abspath(linux_stringa+'data/team_data/')):
-        if filename.endswith(".csv"):
-            diff = avg_diff.difference_average(filename)
-            print(diff)
-            writer.writerow({"Teams": filename.split('.')[0], "Avg Point Diff": diff})
-        else:
-            continue
+# REMOVE COMMENTS BELOW FOR CSV DUMP OF AVERAGE TEAM DIFFERENTIALS
+
+# with open('team_avg_diffs.csv', 'w') as dump:
+#     fieldnames = ["Teams", "Avg Point Diff"]
+#     writer = csv.DictWriter(dump, fieldnames=fieldnames)
+#     linux_stringa = ''
+#     linux_stringb = ''
+#     if platform.system() == 'Linux':
+#         linux_stringa = '../'
+#     writer.writeheader()
+#     for filename in os.listdir(os.path.abspath(linux_stringa+'data/team_data/')):
+#         if filename.endswith(".csv"):
+#             diff = avg_diff.difference_average(filename)
+#             print(diff)
+#             writer.writerow({"Teams": filename.split('.')[0], "Avg Point Diff": diff})
+#         else:
+#             continue
+
+SRS.SRS('data/team_avg_diffs.csv', 'ABILENECHRISTIAN')
