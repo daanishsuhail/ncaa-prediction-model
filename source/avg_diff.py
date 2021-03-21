eebd7302a69a85aa993cd282346b0aba6fa71ecd
Daanish <dhsh112@protonmail.com>
2021-03-20 20:52:26 -0500

    functionalized the avg point difference into the main file

--- source/avg_diff.py
+++ source/avg_diff.py
@@ -1,22 +1,24 @@
 import csv
-print("opening csv file")
 
-with open('../data/team_data/RUTGERS.csv', 'r') as file:
-    reader = csv.reader(file)
-    abilene_test = list(reader)
-    total_file_rows = len(list(open("../data/team_data/RUTGERS.csv")))
-    row = 1
-    column = 8
-    number_of_games = 0
-    point_diff = 0
-    avg_point_diff = 0.0
-    while row < total_file_rows:
-        tmm_pts = float(abilene_test[row][column])
-        opp_pts = float(abilene_test[row][column+1])
-        print(tmm_pts - opp_pts)
-        point_diff += (tmm_pts - opp_pts)
-        row += 1
-        if row == total_file_rows:
-            print(point_diff)
-            avg_point_diff = float(point_diff)/(row-1)
-            print(avg_point_diff)
+def difference_average():
+    print("opening csv file")
+
+    with open("../data/team_data/RUTGERS.csv", 'r') as file:
+        reader = csv.reader(file)
+        abilene_test = list(reader)
+        total_file_rows = len(list(open("../data/team_data/RUTGERS.csv")))
+        row = 1
+        column = 8
+        number_of_games = 0
+        point_diff = 0
+        avg_point_diff = 0.0
+        while row < total_file_rows:
+            tmm_pts = float(abilene_test[row][column])
+            opp_pts = float(abilene_test[row][column+1])
+            print(tmm_pts - opp_pts)
+            point_diff += (tmm_pts - opp_pts)
+            row += 1
+            if row == total_file_rows:
+                print(point_diff)
+                avg_point_diff = float(point_diff)/(row-1)
+                print(avg_point_diff)
--- source/main.py
+++ source/main.py
@@ -1,5 +1,6 @@
+import avg_diff
 # this is the master file where all functions are going to be run.
 # do as little work as possible on this file, we'll can call them by classes later.
 
 print("hello world")
-
+avg_diff.difference_average()
