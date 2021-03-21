35269e365d7c4157a121068d053f001b071e385f
Camyo4650 <campilchard@gmail.com>
2021-03-21 15:21:40 -0500

    Update main.py

--- source/main.py
+++ source/main.py
@@ -3,6 +3,7 @@ import SRS
 import sys
 import os
 import csv
+import platform
 
 # this is the master file where all functions are going to be run.
 # do as little work as possible on this file, we'll can call them by classes later.
@@ -12,21 +13,18 @@ import csv
 
 # differences - A dictionary set with the key as the teamcode and the value as the average point differential
 
-# REMOVE COMMENTS UNDERNEATH TO DUMP THE AVG DIFF CSV
-
-# Change
-
-# with open('data/team_avg_diffs.csv', 'w') as dump:
-#     fieldnames = ["Teams", "Avg Point Diff"]
-#     writer = csv.DictWriter(dump, fieldnames=fieldnames)
-
-#     writer.writeheader()
-#     for filename in os.listdir(os.path.abspath('data/team_data/')):
-#         if filename.endswith(".csv"):
-#             diff = avg_diff.difference_average(filename)
-#             print(diff)
-#             writer.writerow({"Teams": filename.split('.')[0], "Avg Point Diff": diff})
-#         else:
-#             continue
-
-SRS.SRS('data/team_avg_diffs.csv', "ABILENECHRISTIAN")
+with open('team_avg_diffs.csv', 'w') as dump:
+    fieldnames = ["Teams", "Avg Point Diff"]
+    writer = csv.DictWriter(dump, fieldnames=fieldnames)
+    linux_stringa = ''
+    linux_stringb = ''
+    if platform.system() == 'Linux':
+        linux_stringa = '../'
+    writer.writeheader()
+    for filename in os.listdir(os.path.abspath(linux_stringa+'data/team_data/')):
+        if filename.endswith(".csv"):
+            diff = avg_diff.difference_average(filename)
+            print(diff)
+            writer.writerow({"Teams": filename.split('.')[0], "Avg Point Diff": diff})
+        else:
+            continue
