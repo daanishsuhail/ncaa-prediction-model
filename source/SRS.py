b14e259ad8a7fb693ef035fd8d98585c6b95bbf7
Camyo4650 <campilchard@gmail.com>
2021-03-21 16:56:18 -0500

    SRS calculation to be continued

--- source/SRS.py
+++ source/SRS.py
@@ -2,14 +2,21 @@ import csv
 
 # despite what the file name may suggest in statistics, in this project, SRS is an abbreviation for Simple Rating System.
 
-avg_diffs = {}
-def SRS(team_avg_diffs, team):
-    tad = open(team_avg_diffs, mode='r')
-    reader = csv.DictReader(tad)
+# this is the function responsible for actually calculating every SRS
+
+
+def calcSRS(atdDict):
+    d = dict()
+    for k, v in atdDict:
+        
+    return d
+
+
+def SRS(avg_team_diffs):
+    atd = open(avg_team_diffs, mode='r')
+    reader = csv.DictReader(atd)
     l = list(reader)
-    avg_diffs = dict()
+    atdDict = dict()
     for d in l:
         avg_diffs[d["Teams"]] = d["Avg Point Diff"]
-    # initial value
-    init = avg_diffs[team]
-    print(init)
+    
