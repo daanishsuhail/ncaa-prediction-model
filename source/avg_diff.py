de4ec4dd4739c8017a5907a7f1a50d78092fec9e
Daanish <dhsh112@protonmail.com>
2021-03-20 20:40:46 -0500

    enabled the printing of a final average value

--- source/avg_diff.py
+++ source/avg_diff.py
@@ -1,19 +1,22 @@
 import csv
 print("opening csv file")
 
-with open('../data/team_data/ABILENECHRISTIAN.csv', 'r') as file:
+with open('../data/team_data/RUTGERS.csv', 'r') as file:
     reader = csv.reader(file)
     abilene_test = list(reader)
-    total_file_rows = len(list(open("../data/team_data/ABILENECHRISTIAN.csv")))
+    total_file_rows = len(list(open("../data/team_data/RUTGERS.csv")))
     row = 1
     column = 8
     number_of_games = 0
     point_diff = 0
+    avg_point_diff = 0.0
     while row < total_file_rows:
-        tmm_pts = int(abilene_test[row][column])
-        opp_pts = int(abilene_test[row][column+1])
+        tmm_pts = float(abilene_test[row][column])
+        opp_pts = float(abilene_test[row][column+1])
         print(tmm_pts - opp_pts)
         point_diff += (tmm_pts - opp_pts)
         row += 1
         if row == total_file_rows:
             print(point_diff)
+            avg_point_diff = float(point_diff)/(row-1)
+            print(avg_point_diff)
