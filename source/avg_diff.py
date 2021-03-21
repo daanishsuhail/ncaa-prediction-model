2acbc3a308be850b67deb61c900745811e9776a7
Daanish <dhsh112@protonmail.com>
2021-03-20 21:05:34 -0500

    enabled the use of user input

--- source/avg_diff.py
+++ source/avg_diff.py
@@ -1,9 +1,12 @@
 import csv
 
-def difference_average():
-    print("opening csv file")
 
-    with open("../data/team_data/RUTGERS.csv", 'r') as file:
+# Computes the average difference
+
+def difference_average(str):
+    # print("opening csv file") REMOVE COMMENTS TO ENABLE SEEING THE "open csv file" MEANT FOR DEBUGGING
+    team = str; 
+    with open("../data/team_data/"+team+".csv", 'r') as file:
         reader = csv.reader(file)
         abilene_test = list(reader)
         total_file_rows = len(list(open("../data/team_data/RUTGERS.csv")))
@@ -15,10 +18,10 @@ def difference_average():
         while row < total_file_rows:
             tmm_pts = float(abilene_test[row][column])
             opp_pts = float(abilene_test[row][column+1])
-            print(tmm_pts - opp_pts)
+            # print(tmm_pts - opp_pts) REMOVE THE COMMENTS TO SEE THE INDIVIDUAL POINT DIFFERENCE OF EACH GAME
             point_diff += (tmm_pts - opp_pts)
             row += 1
             if row == total_file_rows:
-                print(point_diff)
+                # print(point_diff) REMOVE THE COMMENTS TO SEE THE TOTALED POINT DIFFERENCES
                 avg_point_diff = float(point_diff)/(row-1)
-                print(avg_point_diff)
+                print(avg_point_diff) # PRINTS THE AVERAGE POINT DIFFERENCE
--- source/main.py
+++ source/main.py
@@ -2,5 +2,6 @@ import avg_diff
 # this is the master file where all functions are going to be run.
 # do as little work as possible on this file, we'll can call them by classes later.
 
-print("hello world")
-avg_diff.difference_average()
+# function finds the average point difference. See avg_diff.py for more
+bball = raw_input(str) # USE raw_input() for Python 2.x and input() for Python 3.x
+avg_diff.difference_average(str(bball))
