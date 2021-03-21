6b2f189c66f3596fe67fd9e68f0e30c85a547540
unknown <campilchard@gmail.com>
2021-03-21 13:55:51 -0500

    Changed input to a for loop

--- source/avg_diff.py
+++ source/avg_diff.py
@@ -7,10 +7,10 @@ from os import path
 def difference_average(str):
     # print("opening csv file") REMOVE COMMENTS TO ENABLE SEEING THE "open csv file" MEANT FOR DEBUGGING
     team = str; 
-    with open(path.abspath('data/team_data/'+team+'.csv'), 'r') as file:
+    with open(path.abspath('data/team_data/'+team), 'r') as file:
         reader = csv.reader(file)
         abilene_test = list(reader)
-        total_file_rows = len(list(open(path.abspath('data/team_data/'+team+'.csv'))))
+        total_file_rows = len(list(open(path.abspath('data/team_data/'+team))))
         row = 1
         column = 8
         number_of_games = 0
--- source/main.py
+++ source/main.py
@@ -1,5 +1,6 @@
 import avg_diff
 import sys
+import os
 
 # this is the master file where all functions are going to be run.
 # do as little work as possible on this file, we'll can call them by classes later.
@@ -7,13 +8,15 @@ import sys
 
 # function finds the average point difference. See avg_diff.py for more
 
-# USE raw_input() for Python 2.x and input() for Python 3.x
+# differences - A dictionary set with the key as the teamcode and the value as the average point differential
+differences = {}
+print(os.path.abspath('data/team_data/'))
+for filename in os.listdir(os.path.abspath('data/team_data/')):
+    if filename.endswith(".csv"):
+        diff = avg_diff.difference_average(filename)
+        print(diff)
+        differences[filename.split('.')[0]] = diff
+    else:
+        continue
 
-def sysInput(str):
-    if sys.version_info[0] == 2:
-        return raw_input(str(str))
-    if sys.version_info[0] == 3:
-        return input(str(str))
-print("please type in the school code")
-print(avg_diff.difference_average(sysInput(str)))
-print("Average Tmm-Opp value^\n\n")
+print(differences)
